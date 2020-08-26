4a8e1ceaeec962fda6256e95bb9ff013ce7466f5
Alfredo Romero <alfredo.romero@tagle.net.ar>
2020-08-26 15:48:13 -0300

    commit-1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application dashboard. das
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
