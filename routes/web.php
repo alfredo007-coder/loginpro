f08447fd0a5179cd1775c69408d39b59a09685f4
Alfredo Romero <alfredo.romero@tagle.net.ar>
2020-08-26 10:21:19 -0300

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use App\Mail\TestMail;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/presentacion', function () {
+    //return view('welcome');
+    // if (!isset (Auth::user()->email)){
+       
+    //     return("no esta logueado");
+    //     //die;
+    // }
+    // echo(Auth::user()->email);
+    Mail::to("alfredoromero@hotmail.com")->send(new TestMail("alfredo"));
+    return view('welcome');
+    
+})->name('presentacion');
